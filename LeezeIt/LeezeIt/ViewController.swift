eef5344f1fb01972eb25f544acedd240c349afa7
RiffatiKhan <111387186+RiffatiKhan@users.noreply.github.com>
2022-08-24 16:24:59 -0500

    create a project LeezeIt

---
+++ LeezeIt/LeezeIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeezeIt
+//
+//  Created by Riffat Khan on 8/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
