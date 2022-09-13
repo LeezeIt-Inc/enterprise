e616f3ff16058e5d31c3272545ccd556ba237584
harris ali <aliharris484@gmail.com>
2022-09-13 00:49:57 -0400

    Merged Develop into Kas07

--- LeezeIt/LeezeIt/ViewController.swift
+++ LeezeIt/LeezeIt/ViewController.swift
@@ -7,15 +7,116 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    @IBAction func nextScreen(_ sender: Any) {
-        let storyBoard = UIStoryboard(name: "VerificationViewController", bundle: nil)
-        let vc = storyBoard.instantiateViewController(withIdentifier: "VerificationViewController") as! VerificationViewController
-        present(vc, animated: true)
-    }
+
+class ViewController: UIViewController {
+    
+    let parentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 190
+        return stackView
+    }()
+    
+    let leezeitImageStackView:  UIStackView = {
+        let stackView = UIStackView()
+        return stackView
+        
+    }()
+    
+    let buttonStackView:  UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 30
+        return stackView
+    }()
+    
+    var leezeitImage: UIImageView = {
+        let leezeitImage =  UIImageView(image: .init(named: "LeezeitImage"))
+        leezeitImage.contentMode                               = .scaleToFill
+        leezeitImage.clipsToBounds                             = true
+        return leezeitImage
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addViewsInView()
+        
+    } 
+
+    @objc func logIn(_ sender: UIButton) {
+        print("LogIn Button Tapped")
+        
+    }
+    
+    @objc func signUP(_ sender: UIButton) {
+        print("signUp Button Tapped")
+    }
+}
+
+extension ViewController {
+ 
+    func parentStackViewConstraint() {
+        parentStackView.translatesAutoresizingMaskIntoConstraints                                                           = false
+        parentStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 170).isActive           = true
+        parentStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive     = true
+        parentStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive   = true
+        parentStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive     = true
+        //      adding views In StackView.
+        addViewsInParentStackView()
+        
+    }
+    
+    func imageStackViewConstraint() {
+        leezeitImageStackView.translatesAutoresizingMaskIntoConstraints                                               = false
+        leezeitImageStackView.centerXAnchor.constraint(equalTo: parentStackView.centerXAnchor).isActive               = true
+        leezeitImageStackView.topAnchor.constraint(equalTo: parentStackView.bottomAnchor, constant: -10).isActive     = true
+        leezeitImageStackView.addArrangedSubview(leezeitImage)
+        
+        leezeitImage.translatesAutoresizingMaskIntoConstraints                                               = false
+        leezeitImage.heightAnchor.constraint(equalTo: parentStackView.widthAnchor, multiplier: 0.4).isActive = true
+    }
+    
+    func buttonStackViewConstraint() {
+        buttonStackView.translatesAutoresizingMaskIntoConstraints                                                          = false
+        buttonStackView.leadingAnchor.constraint(equalTo: parentStackView.leadingAnchor, constant: 35).isActive            = true
+        buttonStackView.trailingAnchor.constraint(equalTo: parentStackView.trailingAnchor, constant: -35).isActive         = true
+        buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -130).isActive    = true
+
+        addButtonsToStackView()
+    }
+   
+    func addViewsInParentStackView() {
+        parentStackView.addArrangedSubview(leezeitImageStackView)
+        imageStackViewConstraint()
+        parentStackView.addArrangedSubview(buttonStackView)
+        buttonStackViewConstraint()
+    }
+    
+    func addViewsInView() {
+        view.addSubview(parentStackView)
+        parentStackViewConstraint()
+    }
+    
+    func addButtonsToStackView() {
+        let numOfButtons = 2
+        
+        for i in 1...numOfButtons {
+            
+            let button = CreateButton()
+            
+            if i == 1 {
+                button.setTitle("Log In", for: .normal)
+                button.addTarget(self, action: #selector(logIn(_:)), for: .touchUpInside)
+                buttonStackView.addArrangedSubview(button)
+            } else {
+                button.setTitle("Sign Up", for: .normal)
+                button.addTarget(self, action: #selector(signUP(_:)), for: .touchUpInside)
+                buttonStackView.addArrangedSubview(button)
+            }
+        }
     }
 }
