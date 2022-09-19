b375901d4972f6f14c255f9704f82b6aa531d81d
harris ali <aliharris484@gmail.com>
2022-09-19 04:58:00 -0400

    add  button reuseable

---
+++ LeezeIt/Custom_Button.swift
@@ -0,0 +1,27 @@
+//
+//  Custom_Button.swift
+//  LeezeIt
+//
+//  Created by harris ali on 9/19/22.
+//
+
+import Foundation
+import UIKit
+
+class SharedClass: NSObject {
+    
+    static let sharedInstance = SharedClass()
+    
+    func styleForComponents(button: UIButton) {
+        button.layer.cornerRadius = 21
+        button.frame.size = CGSize(width: 350, height: 60)
+        button.backgroundColor = .systemMint
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        
+        
+    }
+    
+    private override init() {
+        
+    }
+}
